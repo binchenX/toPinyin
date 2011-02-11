ab11508b2af422db0ae7783bceb846c55355c52c
pierrchen <pierr.chen@gmail.com>
2011-02-11 17:34:45 +0800

    improve test cases

--- lib/test.rb
+++ lib/test.rb
@@ -2,10 +2,10 @@
 require 'toPinyin'
 
 s = "春花秋月何时了"
-puts s.utf8?
-puts s.utf8.inspect
-puts s.pinyin.inspect
-puts s.unicode.inspect
+#puts s.utf8?
+raise "toUTF8 faild" if  s.utf8 !=  ["e698a5", "e88ab1", "e7a78b", "e69c88", "e4bd95", "e697b6", "e4ba86"]
+raise "toPinyin faild" if s.pinyin != ["chun", "hua", "qiu", "yue", "he", "shi", "liao"]
+raise "toUnicode failed" if s.unicode != ["6625", "82B1", "79CB", "6708", "4F55", "65F6", "4E86"]
 
 
 #let's sort it 
@@ -20,10 +20,19 @@ words = "
 你的
 平台".split("\n")
 
-puts words 
-
-words.each {|w| puts w.pinyin.inspect ; puts w.unicode.inspect ; puts w.utf8.inspect}
+#for debug
+#words.each {|w| puts w.pinyin.inspect ; puts w.unicode.inspect ; puts w.utf8.inspect}
 words.sort! {|a ,b|   a.pinyin.join <=> b.pinyin.join }
 
-puts words
+sorted = "
+的二
+检查
+进制
+你的
+平台
+适合
+是否
+我们".split("\n")
+
+raise "sort failed" if words.join != sorted.join
 

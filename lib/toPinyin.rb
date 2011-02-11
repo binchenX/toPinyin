98a61c2cc9dc958e5151050866243d0852c24439
pierrchen <pierr.chen@gmail.com>
2011-02-11 15:05:52 +0800

    first check in

--- lib/toPinyin.rb
+++ lib/toPinyin.rb
@@ -1,3 +1,18 @@
+
+require 'iconv'
+require './toPinyin/uniToPyMap'
 module Topinyin
-  # Your code goes here...
+
+def self.toPinyin(char,encoding="utf-8")
+#conver to unicode
+u = Iconv.iconv("UNICODEBIG",encoding,char)[0].each_byte.map {|b| b.to_s(16)}.join
+#find the pinyin
+uniToPyMap[u.upcase]
+end
+
+
+puts Topinyin.toPinyin "中"
+
+
+
 end
